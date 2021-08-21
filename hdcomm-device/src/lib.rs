5e31aa2f9b45914c1277491b1b7f330de314b5de
Shenghao Yang <me@shenghaoyang.info>
2021-08-21 15:24:23 +0800

    hdcomm-device: add wrapper around to_slice_cobs

--- hdcomm-device/src/lib.rs
+++ hdcomm-device/src/lib.rs
@@ -24,3 +24,22 @@ impl Accumulator {
         self.0.feed(buf)
     }
 }
+
+/// Maximum number of bytes required for a serialized `Message` framed using
+/// COBS.
+///
+/// Calculated as ceil(MAX_MESSAGE_LENGTH / 254).
+pub const ENCODED_BUFFER_SIZE: usize = (hdcomm_core::MAX_MESSAGE_LENGTH / 254)
+    + if (hdcomm_core::MAX_MESSAGE_LENGTH % 254) > 0 {
+        1
+    } else {
+        0
+    };
+
+/// Serializes a `Message` and writes its COBS-framed version to a buffer.
+///
+/// This buffer should be at least `ENCODED_BUFFER_SIZE` in length to avoid
+/// errors due to insufficient outptu buffer space.
+pub fn frame<'a>(message: &Message, buf: &'a mut [u8]) -> Result<&'a mut [u8], postcard::Error> {
+    postcard::to_slice_cobs(message, buf)
+}
