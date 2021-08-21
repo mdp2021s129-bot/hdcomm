mdp2021s129-bot/hdcomm

commit 5e31aa2f9b45914c1277491b1b7f330de314b5de

hdcomm-device: add wrapper around to_slice_cobs

hdcomm-device/src/lib.rs
@@ -24,3 +24,22 @@ impl Accumulator {
         self.0.feed(buf)
     }
 }
+
+/// Maximum number of bytes required for a serialized `Message` framed using
+/// COBS.
+///
+/// Calculated as ceil(MAX_MESSAGE_LENGTH / 254).
+pub const ENCODED_BUFFER_SIZE: usize = (hdcomm_core::MAX_MESSAGE_LENGTH / 254)
+    + if (hdcomm_core::MAX_MESSAGE_LENGTH % 254) > 0 {
+        1
+    } else {
+        0
+    };
+
+/// Serializes a `Message` and writes its COBS-framed version to a buffer.
+///
+/// This buffer should be at least `ENCODED_BUFFER_SIZE` in length to avoid
+/// errors due to insufficient outptu buffer space.
+pub fn frame<'a>(message: &Message, buf: &'a mut [u8]) -> Result<&'a mut [u8], postcard::Error> {
+    postcard::to_slice_cobs(message, buf)
+}

commit 3b1ff5d822925ee222a2be46af0a08cddcae8f4b

hdcomm-device: correct buffer size
Previous commit did not include the message length to the calculated padding.

fixes: 5e31aa2f9b45914c1277491b1b7f330de314b5de

hdcomm-device/src/lib.rs
@@ -29,7 +29,8 @@ impl Accumulator {
 /// COBS.
 ///
 /// Calculated as ceil(MAX_MESSAGE_LENGTH / 254).
-pub const ENCODED_BUFFER_SIZE: usize = (hdcomm_core::MAX_MESSAGE_LENGTH / 254)
+pub const ENCODED_BUFFER_SIZE: usize = hdcomm_core::MAX_MESSAGE_LENGTH
+    + (hdcomm_core::MAX_MESSAGE_LENGTH / 254)
     + if (hdcomm_core::MAX_MESSAGE_LENGTH % 254) > 0 {
         1
     } else {