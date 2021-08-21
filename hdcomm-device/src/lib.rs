22a3e11a07b0ec10b5db0d89002b1369a812f8a0
Shenghao Yang <me@shenghaoyang.info>
2021-08-21 19:04:06 +0800

    hdcomm-device: add an into_vec function for serializing into an existing HVec

--- hdcomm-device/src/lib.rs
+++ hdcomm-device/src/lib.rs
@@ -1,6 +1,12 @@
 #![no_std]
+use core::ops::{Index, IndexMut};
+
 use hdcomm_core::message::Message;
-use postcard::{CobsAccumulator, FeedResult};
+use postcard::{
+    flavors::{Cobs, SerFlavor},
+    CobsAccumulator, FeedResult,
+};
+use serde::Serialize;
 
 /// An accumulator that consumes message data and returns deserialized
 /// `Message`s.
@@ -43,4 +49,53 @@ pub const ENCODED_BUFFER_SIZE: usize = hdcomm_core::MAX_MESSAGE_LENGTH
 /// errors due to insufficient outptu buffer space.
 pub fn frame<'a>(message: &Message, buf: &'a mut [u8]) -> Result<&'a mut [u8], postcard::Error> {
     postcard::to_slice_cobs(message, buf)
+}
+
+/// The `HVecRef` flavor is a wrapper type around a reference to a `heapless::Vec`.
+pub struct HVecRef<'a, const B: usize>(&'a mut heapless::Vec<u8, B>);
+
+impl<const B: usize> IndexMut<usize> for HVecRef<'_, B> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+impl<const B: usize> Index<usize> for HVecRef<'_, B> {
+    type Output = u8;
+
+    fn index(&self, idx: usize) -> &u8 {
+        &self.0[idx]
+    }
+}
+/// `SerFlavor` implementation for `HVecRef`.
+///
+/// Does not return anything because the caller is expected to have access
+/// to the target vector.
+impl<const B: usize> SerFlavor for HVecRef<'_, B> {
+    type Output = ();
+
+    #[inline(always)]
+    fn try_extend(&mut self, data: &[u8]) -> core::result::Result<(), ()> {
+        self.0.extend_from_slice(data)
+    }
+
+    #[inline(always)]
+    fn try_push(&mut self, data: u8) -> core::result::Result<(), ()> {
+        self.0.push(data).map_err(|_| ())
+    }
+
+    fn release(self) -> core::result::Result<Self::Output, ()> {
+        Ok(())
+    }
+}
+
+/// Serializes a `Message` into an existing heapless Vec.
+pub fn into_vec<T, const N: usize>(
+    value: &T,
+    result: &mut heapless::Vec<u8, N>,
+) -> postcard::Result<()>
+where
+    T: Serialize + ?Sized,
+{
+    postcard::serialize_with_flavor::<T, _, _>(value, Cobs::try_new(HVecRef(result))?)
 }
