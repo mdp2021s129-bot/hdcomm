e0c8e9f817f115333b4ccb7c510ef0388855ea9a
Shenghao Yang <me@shenghaoyang.info>
2021-09-09 16:59:45 +0800

    stream: add AHRS sample stream type

--- hdcomm-core/src/stream.rs
+++ hdcomm-core/src/stream.rs
@@ -9,4 +9,24 @@ pub struct Message {
 
 /// Payload of a stream message.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub enum Payload {}
+pub enum Payload {
+    /// Payload contains an AHRS sample.
+    Ahrs(AhrsBody),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+/// Ahrs sample.
+///
+/// All arrays are `[x, y, z]`.
+///
+/// Interpretation of the values are left up to uppers layers.
+pub struct AhrsBody {
+    /// Acceleration.
+    pub acc: [i16; 3],
+    /// Angular velocity.
+    pub gyro: [i16; 3],
+    /// Magnetometer.
+    pub mag: [i16; 3],
+    /// Device timestamp.
+    pub time_ms: u32,
+}
