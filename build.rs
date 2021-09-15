e2dcdd938ced4f7c1872a05ccbd5ebf0633fc0a2
Shenghao Yang <me@shenghaoyang.info>
2021-09-15 22:12:36 +0800

    build.rs: rerun only when proto file changes

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=proto/hdcomm.proto");
     tonic_build::compile_protos("proto/hdcomm.proto")?;
     Ok(())
 }
